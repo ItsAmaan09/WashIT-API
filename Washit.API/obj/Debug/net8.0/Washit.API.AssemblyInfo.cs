38a42abf59fa3748980be04f75eab2914ea10469
mohammed amaan <amaanvisiontech@gmail.com>
2025-12-01 15:04:47 +0530

    update files

--- Washit.API/obj/Debug/net8.0/Washit.API.AssemblyInfo.cs
+++ Washit.API/obj/Debug/net8.0/Washit.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Washit.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd3cdd79ab681fe0eb15090c12a37681ee88ece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ebae55073b37f78f81c70bffd36ac2050169b21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Washit.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Washit.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
